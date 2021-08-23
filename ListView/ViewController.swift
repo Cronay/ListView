21493516b0918c05f3444b7a611d6ee7bbad1d26
Cronay <inf@yannic-borgfeld.de>
2021-08-23 22:11:14 +0200

    Make example use of list view

--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let listView = ListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(listView)
+        listView.backgroundColor = .red
+        
+        listView.reload(sectionDimensions: [3]) { index, listView in
+            let label = listView.dequeueRow(type: UILabel.self, at: index)
+            label.backgroundColor = .blue
+            label.text = "Section: \(index.section), Row: \(index.row)"
+            return label
+        }
     }
 
-
+    override func viewWillLayoutSubviews() {
+        listView.frame = view.bounds.inset(by: view.safeAreaInsets).insetBy(dx: 16, dy: 16)
+    }
 }
+
+extension UILabel: RowView {}
 
